48a03fdc897bc3969ff745906f1671a589e4983d
Oswaldo Neto <oswaldo@Oswaldos-MacBook-Pro.local>
2022-02-11 14:10:14 -0300

    code refactored

--- location-simulator/main.py
+++ location-simulator/main.py
@@ -1,4 +1,6 @@
 
+import json
+from uuid import uuid4
 
 from datetime import datetime, timedelta, timezone
 from time import sleep
@@ -6,15 +8,13 @@ from time import sleep
 from nmeasim.simulator import Simulator
 from nmeasim.models import TZ_LOCAL
 
-def print_gps():
 
+def generate_sentences(n):
 
     sim = Simulator()
 
     with sim.lock:
-        # sim.gps.output = ('GGA', 'GLL', 'GSA', 'GSV', 'RMC', 'VTG', 'ZDA')
         sim.gps.output = ('RMC',)
-        sim.gps.num_sats = 14
         # araraquara lat lon
         sim.gps.lat = -21.79
         sim.gps.lon = -48.17
@@ -23,34 +23,22 @@ def print_gps():
         sim.gps.mag_heading = 190.1
         sim.gps.date_time = datetime.now(TZ_LOCAL)
 
+    return list(sim.get_output(n))        
+
+def persist_data(data):
+    with open('locations.json', 'w') as file:
+        json.dump(data, file)
 
 
-    file = open('locations.nmea', 'w')
+def simulate():
 
-    for sentence in list(sim.get_output(100)):
-        file.write(sentence)
-        file.write('\n')
-        print(sentence)
-        # sleep(1)
+    data = {
+        "DeviceId": str(uuid4()),
+        "NMEASentences": generate_sentences(100) 
+    }
 
-
-    file.close()
-
-
-
-
-
-
-
-# gps = GpsReceiver(
-    #     date_time=datetime(2022, 2, 12, 12, 34, 56, tzinfo=timezone.utc),
-    #     output=('RMC',)
-    # )
-    #
-    # for i in range(1000):
-    #     gps.date_time += timedelta(seconds=1)
-    #     print(gps.get_output())
+    persist_data(data)
 
 if __name__ == '__main__':
-    print_gps()
+    simulate()
 
