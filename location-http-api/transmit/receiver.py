c0e60b807cf28eb25524b8ff683e54661083b0c4
Oswaldo Neto <oswaldo@Oswaldos-MacBook-Pro.local>
2022-02-11 14:07:05 -0300

    http body ajusted to expected format

--- location-http-api/transmit/receiver.py
+++ location-http-api/transmit/receiver.py
@@ -2,9 +2,15 @@
 import json
 
 def receive(event, context):
+
+    if not "body" in event or not event["body"]:
+        return {
+            "statusCode": 422
+        }
+
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "helo world",
+            "message": event,
         }),
     }
