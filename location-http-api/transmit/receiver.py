2ae4c9ab0074e40cecf398c1571bf35f4f63099a
Oswaldo Neto <oswaldo@Oswaldos-MacBook-Pro.local>
2022-02-12 09:44:42 -0300

    making reqtest processing asynchronous through an sqs queue

--- location-http-api/transmit/receiver.py
+++ location-http-api/transmit/receiver.py
@@ -1,16 +1,32 @@
 
 import json
+import boto3
+import os
+
+sqs = boto3.resource('sqs')
+
+RECEIVE_QUEUE = os.environ['RECEIVE_QUEUE']
 
 def receive(event, context):
 
+    # it validates request body that must be present
     if not "body" in event or not event["body"]:
         return {
             "statusCode": 422
         }
 
+    # it gets data from the body
+    data = event["body"]
+
+    # it gets queue from environment variable 
+    queue = sqs.get_queue_by_name(QueueName=RECEIVE_QUEUE)
+
+    # it sends data to sqs queue 
+    queue.send_message(MessageBody=data)
+
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": event,
+            "message": "Location received successfully.",
         }),
     }
